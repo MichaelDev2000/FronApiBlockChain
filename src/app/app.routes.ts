b3d03a8c9d685f6bdb91679469fbd2e02c338e02
Michael Perez <maicolpr19@gmail.com>
2025-03-15 13:51:42 -0500

    Inyeccion de httpClient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { MembershipCardComponent } from './components/membership-card/membership-card.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'membresias', component: MembershipCardComponent }
+];
---
+++ src/app/components/membership-card/membership-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membership-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './membership-card.component.html',
+  styleUrl: './membership-card.component.css'
+})
+export class MembershipCardComponent {
+
+}
